7ad16b54ab2c080c1ecfba5a068354ee3b4d6de1
jack <jack.wgm@gmail.com>
2014-11-09 22:49:12 +0800

    为soci添加vc项目及后端的config头文件.
    Signed-off-by: jack <jack.wgm@gmail.com>

---
+++ src/soci_backends_config.h
@@ -0,0 +1,16 @@
+//
+// Copyright (C) 2011 Alex Ott <alexott@gmail.com>
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef SOCI_BACKENDS_CONFIG_H
+#define SOCI_BACKENDS_CONFIG_H
+
+#define DEFAULT_BACKENDS_PATH "@CMAKE_INSTALL_PREFIX@/@LIBDIR@"
+// #define DEFAULT_BACKENDS_PATH "."
+#define SOCI_LIB_PREFIX       ""
+#define SOCI_LIB_SUFFIX       ""
+#define SOCI_ABI_VERSION      "3.2.2"
+
+#endif // SOCI_BACKENDS_CONFIG_H
